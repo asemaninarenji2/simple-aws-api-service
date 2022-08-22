25bbb9ace5c575a723e5df1032a0a06c5b0d5166
Moh Mohai <asemaninarenji@gmail.com>
2022-08-22 11:37:11 +1000

    proj init

---
+++ src/main/java/com/mainapp/mo2/simpleapi/controllers/TestControllerAPI.java
@@ -0,0 +1,19 @@
+package com.mainapp.mo2.simpleapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/test")
+public class TestControllerAPI {
+    @GetMapping(value = "/test1/{id}")
+    public String test(@PathParam("id") String id){
+        return "TEST SUCCESS";
+    }
+
+
+
+}
---
+++ src/test/java/com/mainapp/mo2/simpleapi/SimpleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainapp.mo2.simpleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
